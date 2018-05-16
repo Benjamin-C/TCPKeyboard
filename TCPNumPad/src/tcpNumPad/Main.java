07c2bd45e7501cb4bd616b27b698bd992dbe5aac
Benjamin-C <bencrall@gmail.com>
2018-05-16 11:03:07 -0700

    Fix

--- TCPNumPad/src/tcpNumPad/Main.java
+++ TCPNumPad/src/tcpNumPad/Main.java
@@ -2,6 +2,7 @@ package tcpNumPad;
 
 public class Main {
 
+	@SuppressWarnings("unused")
 	public static void main(String args[]) {
 		NumPad n = new NumPad();
 	}
--- TCPNumPad/src/tcpNumPad/NumPad.java
+++ TCPNumPad/src/tcpNumPad/NumPad.java
@@ -2,19 +2,14 @@ package tcpNumPad;
 
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Graphics;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
